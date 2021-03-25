e757101808dca6a2c2486e41609c02dea8ceb081
Bohdan <bogdan25015@gmail.com>
2021-03-25 21:36:06 +0300

    Final version(i hope)

---
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -0,0 +1,22 @@
+package sk.stuba.fei.uim.oop;
+import java.util.Random;
+import java.util.*;
+
+
+import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+
+
+class Main {
+
+    public static void main(String[] args) {
+        new Game(3);
+    }
+}
+
