5ed7b6096d3c9c8bcb18a8d22e749103d8bcaade
Bohdan <bogdan25015@gmail.com>
2021-03-26 16:23:27 +0300

    Final version

--- src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
@@ -1,7 +1,22 @@
 package sk.stuba.fei.uim.oop;
+import java.util.Random;
+import java.util.*;
+
+
+import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+
 
 public class Assignment1 {
+
     public static void main(String[] args) {
-        
+        new Game(3);
     }
 }
+
